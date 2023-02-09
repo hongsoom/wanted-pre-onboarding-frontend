d60dd39a05b84fa7390cd3434f7bb79591eec7dc
hongsoom <hongsoom@kakao.com>
2023-02-09 18:56:57 +0900

    [Feat] 서버와 연결할 객체 headers에 토큰 추가

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_API_URL;
+const access_token = localStorage.getItem('token');
 
 const instance = axios.create({
     baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
+        Authorization: "Bearer " + access_token,
     },
 });
 
